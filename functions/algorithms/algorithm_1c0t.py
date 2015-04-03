4815e5f4a6ab6f5922d5ab0215f754627f7ebe1f
Aaron Yan <aaronyan815@gmail.com>
2015-04-03 16:08:25 -0500

    added testing framework for algorithm_1c0t to simulate arriving guests and waiting times.

--- functions/algorithms/algorithm_1c0t.py
+++ functions/algorithms/algorithm_1c0t.py
@@ -1,5 +1,5 @@
 from classes import chef, guest, restaurant
-import test_functions as tf
+import simulation_general as sg
 import datetime
 import numpy as np
 
@@ -9,5 +9,30 @@ if __name__ == "__main__":
 	chef = chef.Chef(idn = "aaron")
 	restaurant.chefs.append(chef)
 
-	future_guests = tf.create_guests(n=5)
+	future_guests = sg.create_guests(n=5)
 	restaurant.confirmed_guests = [g for g in future_guests]
+
+	current_guest = future_guests[0]
+	future_guests.pop(0)
+	chef.q.append(current_guest)
+	current_guest.wait = current_guest.arrive
+
+	counter = 2
+	print [j.name for j in chef.q]
+	print [j.wait for j in chef.q]
+
+	while counter != 0:
+		counter -= 1
+
+		if not chef.q:
+			break
+		
+		if future_guests:
+			current_guest = future_guests[0]
+
+			# Calculate wait time for customer
+			# Add to chef.q
+
+			future_guests.pop(0)
+		print [j.name for j in chef.q]
+		print [j.wait for j in chef.q]
