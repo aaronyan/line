fb53e92f6e9a81f0e46c5a0ea570a02fec10b991
Aaron Yan <aaronyan815@gmail.com>
2015-04-04 18:15:40 -0500

    added print statements to track states in algorithm_1c0t

--- functions/algorithms/algorithm_1c0t.py
+++ functions/algorithms/algorithm_1c0t.py
@@ -12,23 +12,33 @@ if __name__ == "__main__":
 	future_guests = sg.create_guests(n=5)
 	restaurant.confirmed_guests = [g for g in future_guests]
 
+	# Add the first guest to the chef queue
 	current_guest = future_guests[0]
 	future_guests.pop(0)
 	chef.q.append(current_guest)
 	current_guest.wait = current_guest.arrive
+	print "\nNEW GUEST"
+	print current_guest.name, current_guest.arrive, current_guest.wait
+	print [k.name for k in chef.q]
+	print [k.wait.seconds/60 for k in chef.q]
 
 	counter = 2
-	print [j.name for j in chef.q]
-	print [j.wait for j in chef.q]
 
+	# Serve all the guests
 	while counter != 0:
 		counter -= 1
 
 		if not chef.q:
+			print "\nchef.q is empty"
 			break
 		
 		if future_guests:
 			current_guest = future_guests[0]
+
+			print "\nNEW GUEST"
+			print current_guest.name, current_guest.arrive, current_guest.wait
+			print [k.name for k in chef.q]
+			print [k.wait.seconds/60 for k in chef.q]
 
 			# Calculate wait time for customer
 			# Add to chef.q
