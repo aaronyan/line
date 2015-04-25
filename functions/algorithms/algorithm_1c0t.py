1640499f0af8b6e64b70ec8516ca332c003f1c02
Aaron Yan <aaronyan815@gmail.com>
2015-04-25 17:24:40 -0500

    replaced .wait metadata of Class Guest with .t2s (time to serve) and redefined .wait (time waited by customer in restaurant)

--- functions/algorithms/algorithm_1c0t.py
+++ functions/algorithms/algorithm_1c0t.py
@@ -4,10 +4,10 @@ import datetime
 import numpy as np
 import pandas as pd
 
-# Caclulate guest wait time
-def calc_guest_wait(current_guest, restaurant):
+# Caclulate guest t2s time
+def calc_guest_t2s(current_guest, restaurant):
 	for j,chef in enumerate(restaurant.chefs):
-		# If the chef's queue is empty, the current guest wait time = arrive
+		# If the chef's queue is empty, the current guest t2s time = arrive
 		if chef.q == []:
 			restaurant.chefs[0].q.append(current_guest)
 			if current_guest.arrive > current_guest.orders*chef.prep_time:
@@ -19,67 +19,67 @@ def calc_guest_wait(current_guest, restaurant):
 
 		# Check openings in chef queue
 		for i, guest in enumerate(chef.q):
-			front_wait = guest.wait
+			front_t2s = guest.t2s
 			current_arrive = current_guest.arrive
 			current_prep = current_guest.orders*chef.prep_time
 
 			# If the queue is only one person long or looking at the first person in the queue
 			if i == 0:
-				if current_prep < front_wait and current_arrive < front_wait:
+				if current_prep < front_t2s and current_arrive < front_t2s:
 					# print "guest found an opening in the front!"
 					restaurant.chefs[j].q.insert(i, current_guest)
-					current_guest.wait = current_guest.arrive
-					return current_guest.wait
+					current_guest.t2s = current_guest.arrive
+					return current_guest.t2s
 					# If the queue is two people long
 				elif len(chef.q) != 1:
-					back_wait = chef.q[i+1].wait
-					back_min = back_wait - chef.q[i+1].orders*chef.prep_time
-					if current_prep <= back_min and front_wait + current_prep <= back_min and current_arrive <= back_min:
+					back_t2s = chef.q[i+1].t2s
+					back_min = back_t2s - chef.q[i+1].orders*chef.prep_time
+					if current_prep <= back_min and front_t2s + current_prep <= back_min and current_arrive <= back_min:
 						# print "guest found an opening second in line!"
 						restaurant.chefs[j].q.insert(i+1, current_guest)
-						current_guest.wait = front_wait + current_prep
-						return current_guest.wait
+						current_guest.t2s = front_t2s + current_prep
+						return current_guest.t2s
 				else:
 					# print "guest found an opening in the back!"
 					restaurant.chefs[j].q.insert(i+1, current_guest)
-					if current_arrive <= front_wait:
-						current_guest.wait = front_wait + current_prep
+					if current_arrive <= front_t2s:
+						current_guest.t2s = front_t2s + current_prep
 					else:
-						current_guest.wait = current_arrive
-					return current_guest.wait
+						current_guest.t2s = current_arrive
+					return current_guest.t2s
 			# If the queue is anyone not the first or last person in the queue
 			elif i > 0 and i < (len(chef.q)-1):
-				back_wait = chef.q[i+1].wait
-				back_min = back_wait - chef.q[i+1].orders*chef.prep_time
-				if current_prep <= back_min and front_wait + current_prep <= back_min and current_arrive <= back_min:
+				back_t2s = chef.q[i+1].t2s
+				back_min = back_t2s - chef.q[i+1].orders*chef.prep_time
+				if current_prep <= back_min and front_t2s + current_prep <= back_min and current_arrive <= back_min:
 						# print "guest found an opening second in line with queue > 2!"
 						restaurant.chefs[j].q.insert(i+1, current_guest)
-						current_guest.wait = front_wait + current_prep
-						return current_guest.wait
+						current_guest.t2s = front_t2s + current_prep
+						return current_guest.t2s
 			# If the person is the last person in the queue and/or cannot find an opening in the queue
 			else:
 				# print "guest found an opening in the back!"
 				restaurant.chefs[j].q.insert(i+1, current_guest)
-				if current_arrive <= front_wait:
-					current_guest.wait = front_wait + current_prep
+				if current_arrive <= front_t2s:
+					current_guest.t2s = front_t2s + current_prep
 				else:
-					current_guest.wait = current_arrive
-				return current_guest.wait
+					current_guest.t2s = current_arrive
+				return current_guest.t2s
 
 
 def increment_interval(restaurant):
 	for i, chef in enumerate(restaurant.chefs):
 		for j, guest in enumerate(chef.q):
 			if not restaurant.chefs[i].q[j].served:
-				restaurant.chefs[i].q[j].wait -= restaurant.chefs[i].prep_time
+				restaurant.chefs[i].q[j].t2s -= restaurant.chefs[i].prep_time
 				restaurant.chefs[i].q[j].arrive -= restaurant.chefs[i].prep_time
 
 def clean_up_chef_q(restaurant):
 	index_remove = []
 	for i, chef in enumerate(restaurant.chefs):
 		for j, guest in enumerate(chef.q):
-			# print "wait time for", restaurant.chefs[i].q[j].name, "=", restaurant.chefs[i].q[j].wait
-			if restaurant.chefs[i].q[j].wait == datetime.timedelta(minutes = 0):
+			# print "t2s time for", restaurant.chefs[i].q[j].name, "=", restaurant.chefs[i].q[j].t2s
+			if restaurant.chefs[i].q[j].t2s == datetime.timedelta(minutes = 0):
 				restaurant.chefs[i].q[j].served =  True
 				index_remove.append(j)
 		for k in sorted(index_remove, key=int, reverse=True):
@@ -88,7 +88,7 @@ def clean_up_chef_q(restaurant):
 
 def no_algorithm_time(future_guests, restaurant):
 	
-	# add minutes to wait time to normalize based on index
+	# add minutes to t2s time to normalize based on index
 	for i, guest in enumerate(future_guests):
 		guest.arrive = guest.arrive + datetime.timedelta(minutes=i)
 
@@ -98,24 +98,24 @@ def no_algorithm_time(future_guests, restaurant):
 	# simulate the no algorithm queuing
 	look_at_guests = [g for g in future_guests]
 	front = look_at_guests.pop(0)
-	front.wait = front.arrive + front.orders * restaurant.prep_time
+	front.t2s = front.arrive + front.orders * restaurant.prep_time
 
 	while look_at_guests:
 		focus = look_at_guests.pop(0)
 		focus.prep = focus.orders * restaurant.prep_time
 
-		if focus.arrive > front.wait:
-			focus.wait = focus.arrive + focus.prep
+		if focus.arrive > front.t2s:
+			focus.t2s = focus.arrive + focus.prep
 		else:
-			focus.wait = front.wait + focus.prep
+			focus.t2s = front.t2s + focus.prep
 
 		front = focus
 
 	# print [g.arrive.seconds/60 for g in future_guests]
 	# print [g.orders for g in future_guests]
-	# print [g.wait.seconds/60 for g in future_guests]
+	# print [g.t2s.seconds/60 for g in future_guests]
 
-	return future_guests[-1].wait.seconds/60
+	return future_guests[-1].t2s.seconds/60
 
 def algorithm_time(future_guests, restaurant):
 	# Add the first guest to the chef queue
@@ -125,13 +125,13 @@ def algorithm_time(future_guests, restaurant):
 	chf = restaurant.chefs[0]
 	chf.q.append(current_guest)
 	if current_guest.arrive > current_guest.orders * restaurant.prep_time:
-		current_guest.wait = current_guest.arrive
+		current_guest.t2s = current_guest.arrive
 	else:
-		current_guest.wait = current_guest.orders * restaurant.prep_time
+		current_guest.t2s = current_guest.orders * restaurant.prep_time
 	# print "\ncounter = ", counter
 	# print "NEW GUEST"
 	# print "guests: ", [k.name for k in chef.q]
-	# print "wait: ", [k.wait.seconds/60 for k in chef.q]
+	# print "t2s: ", [k.t2s.seconds/60 for k in chef.q]
 	# print "prep: ", [k.prep.seconds/60 for k in chef.q]
 
 	# Serve all the guests
@@ -147,10 +147,10 @@ def algorithm_time(future_guests, restaurant):
 			# print "\ncounter = ", counter
 			# print "NEW GUEST"
 			# print current_guest.name, current_guest.arrive, current_guest.orders
-			current_guest.wait = calc_guest_wait(current_guest, restaurant)
-			# print "calculated wait time =", current_guest.wait
+			current_guest.t2s = calc_guest_t2s(current_guest, restaurant)
+			# print "calculated t2s time =", current_guest.t2s
 			# print "guests: ", [k.name for k in chef.q]
-			# print "wait: ", [k.wait.seconds/60 for k in chef.q]
+			# print "t2s: ", [k.t2s.seconds/60 for k in chef.q]
 			# print "prep: ", [k.prep.seconds/60 for k in chef.q]
 			clean_up_chef_q(restaurant)
 			future_guests.pop(0)
@@ -158,7 +158,7 @@ def algorithm_time(future_guests, restaurant):
 			# print "\ncounter = ", counter
 			# print "NO MORE"
 			# print "guests: ", [k.name for k in chef.q]
-			# print "wait: ", [k.wait.seconds/60 for k in chef.q]
+			# print "t2s: ", [k.t2s.seconds/60 for k in chef.q]
 			# print "prep: ", [k.prep.seconds/60 for k in chef.q]
 			clean_up_chef_q(restaurant)
 
@@ -191,38 +191,42 @@ if __name__ == "__main__":
 	# print alg_count
 	# print no_alg_count
 
-	sim_num = 100000
+	sim_num = 20
+	sample_num = 100
+	data = pd.DataFrame()
 
-	data = {'alg':[0 for i in range(sim_num)],
-			'no_alg':[0 for i in range(sim_num)]}
-	data = pd.DataFrame(data)
+	for i in range(sim_num):
 
-	for j in range(sim_num):
-		sample_n = np.random.randint(4,11)
-		rest = restaurant.Restaurant()
-		chf = chef.Chef(idn = "aaron")
-		rest.chefs.append(chf)
-		copy_guests = []
+		for j in range(sample_num):
+			sample_n = i + 3
+			rest = restaurant.Restaurant()
+			chf = chef.Chef(idn = "aaron")
+			rest.chefs.append(chf)
+			copy_guests = []
 
-		# future_guests = sg.create_guests(mode = 'case_6')
-		future_guests = sg.create_guests(n=sample_n)
-		sg.copy_guests(future_guests, copy_guests)
+			# future_guests = sg.create_guests(mode = 'case_6')
+			future_guests = sg.create_guests(n=sample_n)
+			sg.copy_guests(future_guests, copy_guests)
 
-		# Compare with the algorithm
-		alg_count = algorithm_time(future_guests, rest)
+			# Compare with the algorithm
+			alg_count = algorithm_time(future_guests, rest)
 
-		# Compare with the no_algorithm
-		no_alg_count = no_algorithm_time(copy_guests, rest)
+			# Compare with the no_algorithm
+			no_alg_count = no_algorithm_time(copy_guests, rest)
 
-		data.loc[j,'alg'] = alg_count
-		data.loc[j,'no_alg'] = no_alg_count
+			alg_col = 'alg'+str(i)
+			no_alg_col ='no_alg'+str(i)
+			diff_col = 'diff'+str(i)
+			percent_col = 'percent'+str(i)
 
-	data['diff'] = data['no_alg']-data['alg']
-	data['percent'] = data['diff']/data['no_alg']*100
-	print data
-	print data.mean()
+			data.loc[j,alg_col] = alg_count
+			data.loc[j,no_alg_col] = no_alg_count
 
-	data.to_csv('alg_1c0t_sim.txt', sep='\t', header=False, index=False)
+		data[diff_col] = data[no_alg_col]-data[alg_col]
+		data[percent_col] = data[diff_col]/data[no_alg_col]*100
+
+
+	data.to_csv('alg_1c0t_sim.txt', sep='\t', header=True, index=False)
 
 
 
--- functions/algorithms/classes/guest.py
+++ functions/algorithms/classes/guest.py
@@ -7,8 +7,9 @@ class Guest(object):
 		self.location = ''
 		self.distance = 0
 		self.arrive = datetime.timedelta(minutes = 0)
-		self.wait = datetime.timedelta(minutes = 0)
+		self.t2s = datetime.timedelta(minutes = 0)
 		self.request = datetime.datetime(2015,1,1,0,0,0)
+		self.wait = datetime.timedelta(minutes = 0)
 		self.q_num = 0
 		self.served = False
 		self.orders = 1
